8fa2e6f96c074468760348c1843cb5299c244f5b
Anurag1096 <anuragqwerty09@outlook.com>
2023-11-08 12:50:13 +0530

    app fix

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home'
 function App() {
-  return (
+  return (<>
+  
+  
     <BrowserRouter basename="/index.html">
     <Routes>
       <Route path="/" element={<Home/>}/>
     </Routes>
   </BrowserRouter>
+  <div>hi</div>
+  </>
   );
 }
 
