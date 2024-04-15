bd1dccf8ab35d79370f2de3f3d14dc39d17ed6fc
Anurag chakravarty <60540331+Anurag1096@users.noreply.github.com>
2024-04-15 19:55:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,16 +6,6 @@ import Home from './pages/Home'
 import DataTable from './components/dataTable';
 function App() {
 
-  return (<>
-  
-  
-    {/* <BrowserRouter basename="/index.html">
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-    </Routes>
-  </BrowserRouter> */}
-  {/* Changed For deployment purpose  */}
-  {/* <div><Home/></div> */}
   <div className=''>
   <DataTable/>
 </div>
